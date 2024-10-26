0e6c81f233f6cf93f2a661faa9672b3b7a8f995d
Lindsey <lindsey_mccluskey@yahoo.ca>
2024-10-25 20:52:56 -0700

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,9 +1,30 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url})}`
+  )
+  next()
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  try {
+    const user = await User.getById(req.params.id)
+    if (!user) {
+      res.status(404).json({
+        message: "No user found with the given ID"
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    })
+  }
 }
 
 function validateUser(req, res, next) {
@@ -15,3 +36,11 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+
+
+module.exports = {
+  logger,
+  validatePost,
+  validateUser,
+  validateUserId
+}
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it
+const server = require('./api/server')
+const port = 5000
+
+server.listen(5000, () => {
+    console.log('magic happening on localhost 5000')
+})
